9085f236d1207388bb924d506803118179e6e6fc
Jaime Zubieta Battista <jaime.zb0@gmail.com>
2014-11-07 22:13:19 -0300

    Rebirth

---
+++ app/controllers/productos_controller.rb
@@ -0,0 +1,40 @@
+class ProductosController < ApplicationController
+  before_action	:getter,		only: [:show, :edit, :update, :destroy]
+  before_action	:new,		only: :create
+  
+  def getter
+    @producto= Producto.find(params[:id])
+  end
+  
+  def new
+    @producto= Producto.new
+  end
+  def index
+    @productos= Producto.search(params[:search])
+  end
+
+  def show
+  end
+  def edit
+  end
+
+  def create
+    @producto.nombre=params[:producto][:nombre]
+    @producto.imagen=params[:producto][:imagen]
+    @producto.descripcion=params[:producto][:descripcion]
+    @producto.save
+    redirect_to productos_path
+  end
+  def update
+    @producto.nombre=params[:producto][:nombre]
+    @producto.imagen=params[:producto][:imagen]
+    @producto.descripcion=params[:producto][:descripcion]
+    @producto.save
+    redirect_to productos_path
+  end
+  def destroy
+    @producto.destroy
+    redirect_to productos_path
+  end
+
+end
