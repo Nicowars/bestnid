922b0149efadd96bc7a707471744f701620d4c13
GabyMorgi <gabymorgi@gmail.com>
2014-11-09 17:12:49 -0300

    reg

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,12 +4,15 @@ class Usuario < ActiveRecord::Base
 	has_many :comentarios
 
 	validates :nombre,
-		presence: true
+		presence: {true, :with => /^[-\w\._]+$/i, :allow_blank =>true, message: "Solo puede contener letras, numeros, o .-_"}
+		lenght: {maximum: 20, message: "el nombre no puede tener más de 20 caracteres"}
 	validates :mail,
-		presence: true,
-		uniqueness: true
+		presence: {true, message: "Campo obligatorio"}
+		uniqueness: {true, mesage: "Este e-mail ya esta registrado"}
+		#formato general del mail
+		VALID_EMAIL_REGEX = /\A[w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+		format: { :with => VALID_EMAIL_REGEX , message: "E-mail no válido"}
 	validates :password,
-		presence: true
-
-	
+		presence: true,
+		confirmation: true
 end
