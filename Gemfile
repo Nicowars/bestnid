2c44b91421d168db037f50f4dfa40e4a7e5f8493
Jaime Zubieta Battista <jaime.zb0@gmail.com>
2014-11-10 14:52:49 -0300

    prueba bootstrap

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # gem 'debugger', group: [:development, :test]
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo'
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
 gem 'hirb'
--- db/schema.rb
+++ db/schema.rb
@@ -50,20 +50,9 @@ ActiveRecord::Schema.define(version: 20141110002549) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "imagen"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
     t.integer  "tel"
     t.string   "alias"
     t.integer  "credit"
   end
-
-  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
 end
