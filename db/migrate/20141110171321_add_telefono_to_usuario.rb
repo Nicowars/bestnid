923d90877c0894f2aaa45d80aaf614e6c6c8d1f4
Nicolas Laurella <nicolaslaurella@hotmail.com>
2014-11-10 15:41:32 -0300

    !Migrate

--- db/migrate/20141110171321_add_telefono_to_usuario.rb
+++
@@ -1,5 +0,0 @@
-class AddTelefonoToUsuario < ActiveRecord::Migration
-  def change
-    add_column :usuarios, :tel, :integer
-  end
-end
--- db/migrate/20141110171347_add_alias_to_usuario.rb
+++
@@ -1,5 +0,0 @@
-class AddAliasToUsuario < ActiveRecord::Migration
-  def change
-    add_column :usuarios, :alias, :string
-  end
-end
