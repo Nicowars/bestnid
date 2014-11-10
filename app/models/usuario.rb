fbcb342656ffed54658fea90ccb713e4d76e0e43
Nicolas Laurella <nicolaslaurella@hotmail.com>
2014-11-10 15:45:31 -0300

    Merge correccion

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,19 +3,10 @@ class Usuario < ActiveRecord::Base
 	has_many :ofertas
 	has_many :comentarios
 
-<<<<<<< HEAD
+
 	validates :alias,
 		presence: true,
 		uniqueness: true
-
-	validates :nombre,
-		presence: true
-
-	validates :mail,
-		uniqueness: true
-
-	validates :password,
-		presence: true
 
 	validates :tel,
 		presence: true,
@@ -24,12 +15,12 @@ class Usuario < ActiveRecord::Base
 	validates :credit,
 		presence: true,
 		uniqueness: true	
-=======
+
 	validates_presence_of :nombre, :mail  
 	validates_presence_of :password, :on => :create  
 	validates_presence_of :nombre, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"  
 	validates_uniqueness_of :nombre, :mail  
 	validates_length_of :password, :minimum => 4, :allow_blank => true  
 	validates_confirmation_of :password
->>>>>>> origin/registro
+
 end
