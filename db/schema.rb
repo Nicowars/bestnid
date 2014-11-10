97d1cab0280fb6aa3b203f00630164294275e174
Nicolas Laurella <nicolaslaurella@hotmail.com>
2014-11-09 21:14:45 -0300

    Migracion Parte 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109230507) do
+ActiveRecord::Schema.define(version: 20141110001217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20141109230507) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "imagen"
+    t.integer  "tel"
+    t.string   "alias"
   end
 
 end
