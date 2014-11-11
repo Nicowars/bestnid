14ded17dcd0ada34ed8c1b41808c58314d7f8d83
alexdmuriel <alex.damian.muriel@gmail.com>
2014-11-11 14:57:01 -0300

    R12A2

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,12 +3,17 @@ class Usuario < ActiveRecord::Base
 	has_many :ofertas
 	has_many :comentarios
 
-	validates_presence_of :nombre, :password
+	validates :nombre,
+		presence: true,
+		format: { :with => /\A[a-zA-Z\s]+\z/ }
+
+	validates :password,
+		presence: true
 
 	validates :alias,
 		presence: true,
 		uniqueness: true,
-		format: { :with => /\A-\._\w+\z/, :message => "pum"}
+		format: { :with => /\A[\w\.-]+\z/ }
 
 	validates :tel,
 		presence: true, 
