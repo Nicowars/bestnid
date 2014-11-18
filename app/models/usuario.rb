40abbea76e7e87985037f7ea6e09b22ccb5bca3c
alexdmuriel <alex.damian.muriel@gmail.com>
2014-11-18 16:29:41 -0300

    sprint2

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -9,11 +9,17 @@ class Usuario < ActiveRecord::Base
 	#password:	presencia
 	#mail:		presencia, 					unico
 	#telefono:	presencia, 			numero, unico
-	#tarjeta: 	presencia, formato, numero,			tamaño
+	#tarjeta: 	presencia, formato, 				tamaño
 	
+	#----Nuevos------------------
+	#domicilio: presencia, ?		?		?		?
+	#titular:	presencia, formato
+	#venci:		presencia, formato
+	
+	#Sacar comentarios post-migraciones
 	validates_uniqueness_of :alias, :mail, :tel, message: "Ya existe"
-	validates_numericality_of :tel, :credit, message: "Debe ser un numero"
-	validates_presence_of :nombre, :password, :alias, :mail, :tel, :credit, message: "Debe completarse"
+	validates_numericality_of :tel, message: "Debe ser un numero"
+	validates_presence_of :nombre, :password, :alias, :mail, :tel, :credit, #:domicilio, message: "Debe completarse"
 	#validates_length_of :credit, minimum: 16, maximum: 16, message: "Debe contener 16 caracteres"
 	validates :nombre,
 		format: { with: /\A[a-zA-Z\s]+\z/, message: "Solo puede tener letras y espacios" }
@@ -21,4 +27,8 @@ class Usuario < ActiveRecord::Base
 		format: { with: /\A[\w\.-]+\z/, message: "Solo puede tener letras, numeros o -, ., _"  }
 	validates :credit,
 		format: { with: /\A[\d]+\z/, message: "Solo puede tener digitos numericos" }
+	#validates :titular,
+		#format: { with: /\A[a-zA-Z\s]+\z/, message: "Solo puede tener letras y espacios" }
+	#validates :domicilio,
+		#format: { with: /\A[\w\s]+\z/, message: "Solo puede tener letras, numeros o espacios"  }
 end
