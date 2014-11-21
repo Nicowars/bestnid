485c32dcf214c6eabce154f5480e55b373f6dff1
alexdmuriel <alex.damian.muriel@gmail.com>
2014-11-20 21:01:57 -0300

    doitnow

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -18,7 +18,7 @@ class Usuario < ActiveRecord::Base
 	#Sacar comentarios post-migraciones
 	validates_uniqueness_of :alias, :mail, :tel, message: "Ya existe"
 	validates_numericality_of :tel, message: "Debe ser un numero"
-	validates_presence_of :nombre, :password, :alias, :mail, :tel, :credit, :domicilio, message: "Debe completarse"
+	validates_presence_of :nombre, :password, :alias, :mail, :tel, :credit, :domicilio, :vencimiento, :titular, message: "Debe completarse"
 	validates_length_of :credit, minimum: 16, maximum: 16, message: "Debe contener 16 caracteres"
 	validates :nombre,
 		format: { with: /\A[a-zA-Z\s]+\z/, message: "Solo puede tener letras y espacios" }
