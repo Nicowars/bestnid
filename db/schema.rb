6470f96c87df3c16937346f8f7803b6fa85c2ae1
alexdmuriel <alex.damian.muriel@gmail.com>
2014-11-22 18:13:10 -0300

    categorias migration 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122194240) do
+ActiveRecord::Schema.define(version: 20141122203525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141122194240) do
     t.datetime "updated_at"
     t.integer  "usuario_id"
     t.date     "vencimiento"
+    t.string   "categoria_id"
   end
 
   create_table "usuarios", force: true do |t|
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 20141122194240) do
     t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "imagen"
     t.integer  "tel"
     t.string   "alias"
     t.string   "domicilio"
