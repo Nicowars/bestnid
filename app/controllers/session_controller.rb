e3b1f834c6e347b613d91e9f9fc725a67e35b577
Nicowars <nicolaslaurella@hotmail.com>
2014-11-22 15:37:55 -0300

    Logueo con reporte de error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,17 +1,18 @@
 class SessionController < ApplicationController
+  
   def new
-    user = Usuario.find_by(alias: params[:alias])  
-  if user && user.password==params[:password]  
-    session[:usuario_id] = user.id  
-    redirect_to root_url, :notice => "Conectado"  
-  else  
-    flash.now.alert = "Alias o contraseña invalida"
-    render "new", :notice => "Conectado"  
-  end 
+
   end
 
   def create
- 
+     user = Usuario.find_by(alias: params[:alias])  
+    if user && user.password==params[:password]  
+      session[:usuario_id] = user.id  
+      redirect_to root_url, :notice => "Conectado"  
+    else  
+      flash[:notice]="Alias o contrasena invalida"
+      render 'new' 
+    end 
   end
 
   def destroy
