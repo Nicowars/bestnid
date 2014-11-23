01e40c7a5b49714143718218ec65bdfc202462a9
alexdmuriel <alex.damian.muriel@gmail.com>
2014-11-23 19:03:36 -0300

    hacer comentario

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -1,2 +1,49 @@
 class ComentariosController < ApplicationController
+  before_action	:get,		only: [:show, :edit, :update, :destroy]
+  before_action	:new,		only: :create
+  
+  def get
+    @comentario= Comentario.find(params[:id])
+  end
+  
+  def new
+    @comentario= Comentario.new
+  end
+  def index
+    @comentarios= Comentario.all
+  end
+
+  def show
+  end
+  def edit
+  end
+
+  def create
+    usu = Usuario.find(params[:comentario][:usuario_id])
+    prod = Producto.find(params[:comentario][:producto_id])
+	if params[:comentario][:pregunta]=="true" 
+		ask = true
+	else
+		ask = false
+	end
+  	@comentario.usuario=usu
+    @comentario.producto=prod
+    @comentario.detalle=params[:comentario][:detalle]
+	@comentario.pregunta=ask
+	@comentario.comentario=!ask
+    @comentario.save
+    redirect_to prod
+  end
+  def update
+  	@comentario.usuario=params[:comentario][:usuario]
+    @comentario.producto=params[:comentario][:producto]
+    @comentario.detalle=params[:comentario][:detalle]
+    @comentario.save
+    redirect_to params[:comentario][:producto]
+  end
+  def destroy
+    @comentario.destroy
+    redirect_to comentarios_path
+  end
+
 end
