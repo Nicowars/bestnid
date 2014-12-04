63f385cb3fd742cd3b61bb44260ec9cef96cf7fd
Nicowars <nicolaslaurella@hotmail.com>
2014-12-04 19:13:55 -0300

    Tildes

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-#encoding: utf-8
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,5 +21,6 @@ module BestnidFromScratch
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.encoding = "utf-8"
   end
 end
