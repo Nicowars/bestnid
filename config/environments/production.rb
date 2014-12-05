4f123f5c63d8fffdcee165dd8a8524b16c3c4355
Jaime Zubieta Battista <jaime.zb0@gmail.com>
2014-12-05 12:33:25 -0300

    Revert "herokut5"
    This reverts commit 6a4bf092680c93006efcc7e30561a76de6cf412e.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,19 +80,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-#  config.action_mailer.perform_deliveries = true
-#  config.action_mailer.raise_delivery_errors = false
-#  config.action_mailer.default :charset => "utf-8"
-#  config.action_mailer.default_url_options = { :host => 'bestnid-alpha.herokuapp.com' }
-#  config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.smtp_settings = {
-#      :address              => 'smtp.gmail.com',
-#      :port                 => 587,
-#      :domain               => 'bestnid-alpha.herokuapp.com',
-#      :user_name            => 'bestnidmail@gmail.com',
-#      :password             => 'ingcomputacion',
-#      :authentication       => :plain,
-#      :enable_starttls_auto => true
-#  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'bestnid-alpha.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'bestnid-alpha.herokuapp.com',
+      :user_name            => 'bestnidmail@gmail.com',
+      :password             => 'ingcomputacion',
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
 
 end
