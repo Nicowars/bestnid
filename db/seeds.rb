331e08535f646c5648daa36979e473adb654302e
Nicowars <nicolaslaurella@hotmail.com>
2014-12-09 22:53:30 -0300

    New newseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -107,8 +107,6 @@ c.save;
 
 p = Producto.find_or_create_by(nombre:'Kriptonita', descripcion:'200 gramos de Kriptonita', imagen:'kriptonita.jpg', usuario: Usuario.find_by(alias: 'cp'), vencimiento: d, categoria: Categoria.find_by(nombre: 'Otros'));
 o = Oferta.find_or_create_by(producto: p, detalle:'Odio a superman y si algún día lo llego a cruzar este producto me vendría al pelo.', monto: 500, usuario: Usuario.find_by(alias: 'mr'));
-p.ganador=o.id
-p.save
 
 p = Producto.find_or_create_by(nombre:'Aceite y Vinagre', descripcion:'200ml de aceite y 300ml de vinagre. No incluye fascos', imagen:'vinagre.jpg', usuario: Usuario.find_by(alias: 'sr'), categoria: Categoria.find_by(nombre: 'Hogar'), vencimiento: d);
 
